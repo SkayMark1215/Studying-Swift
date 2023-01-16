14f3b5fd5f2b41ef5fd6baf5cc9eb2933364eb1e
Pavel Ryabov <business.skaymark@gmail.com>
2023-01-17 02:15:32 +0300

    Added Tasks 2-6

--- lesson_3_tuples.swift
+++ lesson_3_tuples.swift
@@ -10,3 +10,29 @@ import UIKit
 //
 //myData.name = myName
 //myData.age = Int(myAge)
+
+//MARK: Task 2
+
+//var tuple: (Int16, Int16, Int16)
+//let (const1, const2) = (2, 3)
+//
+//tuple.0 = Int16(const1)
+//tuple.1 = Int16(const2)
+//tuple.2 = tuple1.0 + tuple1.1
+
+//MARK: Task 3-5
+
+//var (one, two) = (12, 21)
+//(one, two) = (two, one)
+
+//MARK: Task 6
+
+//var favourite1: (favMovie: String, favNum: Int8) = ("Harry Potter", 21)
+//let const = favourite1
+//typealias Man = (favMovie: String, favNum: Int8)
+//var favourite2: Man = ("Harry Potter 2", 35)
+//var bufForFavourite: Man
+//bufForFavourite = favourite1
+//favourite1 = favourite2
+//favourite2 = bufForFavourite
+//var anotherFavourite = (favourite1.1, favourite2.1, favourite1.1-favourite2.1)
