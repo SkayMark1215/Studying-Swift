8ebb3b0ce34307f24f88993c9c1e923994f3bdaa
Pavel Ryabov <business.skaymark@gmail.com>
2023-01-16 23:52:18 +0300

    Sepatating lessons and adding Task 1 from Lesson 3

---
+++ lesson_3_tuples.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+//MARK: Lesson 3
+
+//MARK: Task 1
+
+//let myName: String = "Paul"
+//var myAge: UInt8 = 21
+//var myData: (name: String, age: Int)
+//
+//myData.name = myName
+//myData.age = Int(myAge)
