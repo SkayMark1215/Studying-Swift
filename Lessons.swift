0ea5dad141b7b4713fd93e3cd43069388fc4d0ad
Pavel Ryabov <business.skaymark@gmail.com>
2023-01-16 21:48:18 +0300

    first commit

---
+++ Lessons.swift
@@ -0,0 +1,167 @@
+import UIKit
+
+//MARK: Lesson 2
+
+//MARK: Task 1
+
+//let distanceKm: Double = 12.2
+//let timeSec: Int = 370
+//
+//var speedMin = (distanceKm * 1000) / (Double(timeSec) / 60)
+//
+//print(speedMin)
+
+//MARK: Task 2
+
+//let numOne = 211
+//let numTwo = 513
+//
+//print(String(numOne) + String(numTwo))
+
+//MARK: Task 3
+
+//let int8: Int8 = -128
+//let uint8: UInt8 = 255
+//
+//print("Int8 min value = \(int8) and UInt8 max value = \(uint8)")
+
+//MARK: Task 4
+
+//var a: Int = 4
+//var b = 3
+//
+//var c = a
+//a = b
+//b = c
+//
+//print("Var a = \(a) \nVar b = \(b)")
+
+//MARK: Task 5
+
+//let paramOne: Float = 12.2
+//var paramTwo = 13.4
+//
+//paramTwo = 15.9
+
+//MARK: Task 6
+
+//let constInt = 18
+//let constFloat: Float = 16.4
+//let constDouble = 5.7
+//
+//var sum = Float(constInt) + constFloat + Float(constDouble)
+//
+//var production = Int(Float(constInt) * constFloat * Float(constDouble))
+//
+//var rest = Int(constFloat) % Int(constDouble)
+//
+//print("sum = \(sum) \n production = \(production) \n rest = \(rest)")
+
+//MARK: Task 7
+
+//var a = 2, b = 3
+//
+//var equation = (a + (4 * b)) * (a - (3 * b)) + a * a
+//
+//print("\(equation)")
+
+//MARK: Task 8
+
+//var c = 1.75, d = 3.25
+//
+//var equation = 2 * (c + 1) * 2 + 3 * (d + 1)
+//
+//print("\(equation)")
+
+//MARK: Task 9
+
+//let length = 3
+//
+//var square = length * length
+//
+//var circumferenceLength = Double(2) * Double.pi * Double(length)
+//
+//var production = Double(square) * circumferenceLength
+
+//MARK: Task 10
+
+//var str = "Hello!"
+//
+//let chrt: Character = "a"
+//
+//var a = 2, b = 3
+//
+//let result = str + String(chrt) + String(a + b)
+//
+//print(result)
+
+//MARK: Task 11
+
+//print("*   *   *\n * * * *\n  *  *")
+
+//MARK: Task 12
+
+//var dayInTheMonth: Int8 = 3
+//
+//var month = "June"
+//
+//var year: Int32 = 2026
+//
+//print("Today is \(year) \(month) \(dayInTheMonth)")
+
+//MARK: Task 13
+
+//var alwaysRight = true, alwaysWrong = false
+//
+//var resAnd = alwaysRight && alwaysWrong
+//
+//var resOr = alwaysRight || alwaysWrong
+//
+//print("\(resAnd)")
+//
+//print("\(resOr)")
+
+//MARK: Task 14
+
+//( ( true && false ) || true ) - true
+//true && false && true || ( true || false ) - true
+//false || ( false || true ) && ( true && false ) - false
+
+//MARK: Task 15
+
+//let myName = "Paul"
+//
+//var myWeight: Float = 60.0
+//
+//var myHeight = 188
+//
+//var myHeightM = Float(myHeight) / 100
+//
+//var IBM = myWeight / Float(myHeightM * myHeightM)
+//
+//print("\(myName) \(IBM)")
+
+//MARK: Task 16
+
+//1 = 4610
+//2 = 5600
+//3 = 4601
+//4 = 4600
+
+//MARK: Task 17
+
+//(5 * 3) - ((4 / 2) * 2)
+
+//MARK: Task 18
+
+//let a: Double = 2.3
+//let b: Double = 2.2
+//let average = Float((a + b)/2)
+
+//MARK: Lesson 3
+
+//MARK: Task 1
+
+let myName: String = "Paul"
+
+var myAge: UInt8 = 21
