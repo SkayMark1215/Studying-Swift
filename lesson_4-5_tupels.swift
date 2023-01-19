9ead6234ef847ccf3d8f9d87818a6b7ce3649657
Pavel Ryabov <business.skaymark@gmail.com>
2023-01-20 02:21:17 +0300

    Added lessons 4-5

---
+++ lesson_4-5_tupels.swift
@@ -0,0 +1,33 @@
+//MARK: Lesson 4-5
+
+//MARK: Task 1
+
+//var range1 = 1...9
+//var range2 = 1..<10
+
+//MARK: Task 2-3
+
+//Range<Int>
+//Error
+//PartialRangeUpTo<Int>
+//ClosedRange<Int>
+
+//MARK: Task 4
+
+//var range = ..<UInt8(10)
+
+//MARK: Task 5-6
+
+//let range = -100...100
+//var item: UInt = 21
+//var v1: Bool = range.contains(Int(item))
+//print(v1)
+
+//MARK: Task 7
+
+//var allBigLetters = "A"..."Z"
+
+//MARK: Task 8
+
+//var range = ...5.0
+//var range1 = 1.0...5.0
